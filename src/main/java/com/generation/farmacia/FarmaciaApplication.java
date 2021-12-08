5f643f87b83c942b20cc91417f6ebae7617f3bb9
Murilo Feitosa <muriloalf@gmail.com>
2021-12-08 15:29:40 -0300

    @MuriloAugusto1 [Task: Corrigindo erro de datasource com ajuda do lindo boaz]

--- src/main/java/com/generation/farmacia/FarmaciaApplication.java
+++ src/main/java/com/generation/farmacia/FarmaciaApplication.java
@@ -1,12 +1,10 @@
 package com.generation.farmacia;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
-
+@SpringBootApplication
 public class FarmaciaApplication {
 
 	public static void main(String[] args) {
